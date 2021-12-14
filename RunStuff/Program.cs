6cdc549276d2aeb017182b571d278ee4b5bc1b5c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-12-14 09:57:06 -0300

    Update example

--- RunStuff/Program.cs
+++ RunStuff/Program.cs
@@ -8,10 +8,11 @@ namespace RunStuff
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            // Create a factory and store in the D.I.
             var mySQLFactory = new Sharp.MySQL.ConnectionFactory("connstring");
+            // Creata an migration instance
             var migration = new Sharp.MySQL.Migration(mySQLFactory);
-
+            // Add or change tables
             var result = migration.Add<Pessoas>()
                                   .Migrate();
 
