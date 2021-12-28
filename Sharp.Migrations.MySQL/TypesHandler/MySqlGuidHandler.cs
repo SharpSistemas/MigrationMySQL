6eb57adce77ef79b71c09206f43b3d3344ef17aa
DaniloOtavio <danilosilva.tecnico@gmail.com>
2021-12-28 14:17:44 -0300

    Guid handler to mysqls db

--- Sharp.Migrations.MySQL/TypesHandler/MySqlGuidHandler.cs
+++ Sharp.Migrations.MySQL/TypesHandler/MySqlGuidHandler.cs
@@ -1,16 +1,28 @@
 ﻿using Dapper;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Sharp.MySQL.Migrations.TypesHandler
 {
+    /// <summary>
+    /// Guid handler for mysqls db
+    /// </summary>
     public class MySqlGuidHandler : SqlMapper.TypeHandler<Guid>
     {
+        /// <summary>
+        /// Parsers a binary value to guid
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public override Guid Parse(object value)
         {
             return new Guid((byte[])value);
         }
+
+        /// <summary>
+        /// Parsers a guid value to binary
+        /// </summary>
+        /// <param name="parameter">Object in bytearray</param>
+        /// <param name="value">Guid to be converted in bytearray</param>
         public override void SetValue(System.Data.IDbDataParameter parameter, Guid value) => parameter.Value = value.ToByteArray();
     }
 }
