a3d78e59cb68fc6e63260aa1dd56a8c04d7614ec
DaniloOtavio <danilosilva.tecnico@gmail.com>
2021-12-28 09:54:12 -0300

    Model Schema Change

---
+++ Sharp.Migrations.MySQL/Core/Models/Schema_Changes.cs
@@ -0,0 +1,20 @@
+﻿using Sharp.MySQL.Migrations.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp.MySQL.Migrations.Core.Models
+{
+    public class Schema_Changes
+    {
+        [PrimaryKey]
+        [TypeFieldBD(typeField: TypeField.INT, NotNull = true)]
+        [AutoIncrement]
+        public int Schema_Id { get; set; }
+
+        [TypeFieldBD(typeField: TypeField.INT, NotNull = true)]
+        public string Schema_Version { get; set; }
+        [TypeFieldBD(TypeField.DATETIME)]
+        public DateTime Schema_Changed { get; set; }
+    }
+}
