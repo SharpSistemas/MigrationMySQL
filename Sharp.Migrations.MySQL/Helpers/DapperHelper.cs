47635ed39e3a03b5fb415a6ec6e636e194bd9b84
DaniloOtavio <danilosilva.tecnico@gmail.com>
2021-12-28 14:17:37 -0300

    Added a helper to dapper

---
+++ Sharp.Migrations.MySQL/Helpers/DapperHelper.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Sharp.MySQL.Migrations.TypesHandler;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp.MySQL.Migrations.Helpers
+{
+    /// <summary>
+    /// Class with dapper helpers
+    /// </summary>
+    public static class DapperHelper
+    {
+        /// <summary>
+        /// Maps a handler to Guids in MySQL, which allows to store guids as BINARY
+        /// </summary>
+        public static void MapMySqlGuidHandler()
+        {
+            SqlMapper.AddTypeHandler(new MySqlGuidHandler());
+            SqlMapper.RemoveTypeMap(typeof(Guid));
+            SqlMapper.RemoveTypeMap(typeof(Guid?));
+        }
+    }
+}
